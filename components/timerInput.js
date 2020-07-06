81ba819ed56e581d9f6ca788236ffed200c20202
Cameron <xwhalex@icloud.com>
2020-07-06 14:06:43 +0800

    CSS Edits

--- components/timerInput.js
+++ components/timerInput.js
@@ -1,133 +1,143 @@
-import React from "react"
+import React from "react";
 import { random } from "../components/random";
 
-
 class TimerInput extends React.Component {
-    render() {
-        return (
-            <div style={{ marginLeft: 10 }}>
-                <p>輸入分鐘</p>
-                <input type="number" value={this.props.value} onChange={this.props.handleChange} required />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2>輸入分鐘</h2>
+        <input
+          type="number"
+          value={this.props.value}
+          onChange={this.props.handleChange}
+          required
+        />
+      </div>
+    );
+  }
 }
 class Timer extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1 style={{ fontSize: 100, marginLeft: 100 }}>{this.props.value}:{this.props.seconds}</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1 style={{ fontSize: 80, marginLeft: 100 }}>
+          {this.props.value}:{this.props.seconds}
+        </h1>
+      </div>
+    );
+  }
 }
 
 class StartButton extends React.Component {
-    render() {
-        return (
-            <div style={{ marginLeft: 130 }}>
-                <button className="btn btn-lg btn-success" disabled={!this.props.value} onClick={this.props.startCountDown}>Start</button>
-            </div>
-
-        );
-    }
+  render() {
+    return (
+      <div style={{ marginLeft: 130 }}>
+        <button
+          className="btn btn-lg btn-success"
+          disabled={!this.props.value}
+          onClick={this.props.startCountDown}
+        >
+          Start
+        </button>
+      </div>
+    );
+  }
 }
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            seconds: '00',
-            value: '',
-            isClicked: false,
-            luckyWinner: "",
-        }
-        this.secondsRemaining;
-        this.intervalHandle;
-        this.handleChange = this.handleChange.bind(this);
-        this.startCountDown = this.startCountDown.bind(this);
-        this.tick = this.tick.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      seconds: "00",
+      value: "",
+      isClicked: false,
+      luckyWinner: "",
+    };
+    this.secondsRemaining;
+    this.intervalHandle;
+    this.handleChange = this.handleChange.bind(this);
+    this.startCountDown = this.startCountDown.bind(this);
+    this.tick = this.tick.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+    });
+  }
+
+  tick() {
+    var min = Math.floor(this.secondsRemaining / 60);
+    var sec = this.secondsRemaining - min * 60;
+
+    this.setState({
+      value: min,
+      seconds: sec,
+    });
+
+    if (sec < 10) {
+      this.setState({
+        seconds: "0" + this.state.seconds,
+      });
     }
 
-    handleChange(event) {
-        this.setState({
-            value: event.target.value
-        })
+    if (min < 10) {
+      this.setState({
+        value: "0" + min,
+      });
     }
 
-    tick() {
-        var min = Math.floor(this.secondsRemaining / 60);
-        var sec = this.secondsRemaining - (min * 60);
-
-        this.setState({
-            value: min,
-            seconds: sec,
-        })
-
-        if (sec < 10) {
-            this.setState({
-                seconds: "0" + this.state.seconds,
-            })
-
-        }
-
-        if (min < 10) {
-            this.setState({
-                value: "0" + min,
-            })
-
-        }
-
-        if (min === 0 & sec === 0) {
-            clearInterval(this.intervalHandle);
-            this.state.luckyWinner = random()
-        }
-
-
-        this.secondsRemaining--
+    if ((min === 0) & (sec === 0)) {
+      clearInterval(this.intervalHandle);
+      this.state.luckyWinner = random();
+      alert(this.state.luckyWinner);
     }
 
-    startCountDown() {
-        this.intervalHandle = setInterval(this.tick, 1000);
-        let time = this.state.value;
-        this.secondsRemaining = time * 60;
-        this.setState({
-            isClicked: true
-        })
+    this.secondsRemaining--;
+  }
+
+  startCountDown() {
+    this.intervalHandle = setInterval(this.tick, 1000);
+    let time = this.state.value;
+    this.secondsRemaining = time * 60;
+    this.setState({
+      isClicked: true,
+    });
+  }
+
+  render() {
+    const clicked = this.state.isClicked;
+    if (clicked) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-4"></div>
+            <div className="col-md-4">
+              <h2> 抽獎時間 </h2>
+              <Timer value={this.state.value} seconds={this.state.seconds} />
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div className="col-md-4">
+            <TimerInput
+              value={this.state.value}
+              handleChange={this.handleChange}
+            />
+            <Timer value={this.state.value} seconds={this.state.seconds} />
+            <StartButton
+              startCountDown={this.startCountDown}
+              value={this.state.value}
+            />
+
+            <h2>{this.state.isClicked && this.state.luckyWinner} </h2>
+          </div>
+        </div>
+      );
     }
-
-    render() {
-        const clicked = this.state.isClicked;
-        if (clicked) {
-            return (
-                <div>
-                    <div className="row">
-                        <div className="col-md-4"></div>
-                        <div className="col-md-4">
-
-                            <p> 抽獎時間 </p>
-                            <Timer value={this.state.value} seconds={this.state.seconds} />
-                        </div>
-                    </div>
-                </div>
-            );
-        } else {
-            return (
-                <div>
-                    <div className="row">
-                        <div className="col-md-4"></div>
-                        <div className="col-md-4">
-                            <TimerInput value={this.state.value} handleChange={this.handleChange} />
-                            <Timer value={this.state.value} seconds={this.state.seconds} />
-                            <StartButton startCountDown={this.startCountDown} value={this.state.value} />
-                            <h2>Lucky Person</h2>
-                            <h2>{this.state.luckyWinner} </h2>
-
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-    }
+  }
 }
-export default App
+export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+
 import App from "../components/timerInput";
 
 export default function Home({ allPostsData }) {
@@ -10,11 +11,16 @@ export default function Home({ allPostsData }) {
         <title> {siteTitle} </title>{" "}
       </Head>
       <div className="container">
-        <p>使用者列表</p>
-        <App></App>
+        <div className="row">
+          <div className="column">
+            <h2>使用者列表</h2>
+          </div>
+          <div className="column">
+            <App></App>
+          </div>
+        </div>
       </div>
       <section className={utilStyles.headingMd}></section>
     </Layout>
   );
 }
-
