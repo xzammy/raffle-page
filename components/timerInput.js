48b54f217e6f76a9f3948d08a9cfb4974973a68e
Cameron <xwhalex@icloud.com>
2020-07-06 13:44:42 +0800

    Timer Component Edits

--- components/timerInput.js
+++ components/timerInput.js
@@ -4,42 +4,30 @@ import { random } from "../components/random";
 
 class TimerInput extends React.Component {
     render() {
-        return ( <
-            div style = {
-                { marginLeft: 10 } } >
-            <
-            p > 輸入分鐘 < /p> <
-            input type = "number"
-            value = { this.props.value }
-            onChange = { this.props.handleChange }
-            required / >
-            <
-            /div>
+        return (
+            <div style={{ marginLeft: 10 }}>
+                <p>輸入分鐘</p>
+                <input type="number" value={this.props.value} onChange={this.props.handleChange} required />
+            </div>
         );
     }
 }
 class Timer extends React.Component {
     render() {
-        return ( <
-            div >
-            <
-            h1 style = {
-                { fontSize: 100, marginLeft: 100 } } > { this.props.value }: { this.props.seconds } < /h1> <
-            /div>
+        return (
+            <div>
+                <h1 style={{ fontSize: 100, marginLeft: 100 }}>{this.props.value}:{this.props.seconds}</h1>
+            </div>
         );
     }
 }
 
 class StartButton extends React.Component {
     render() {
-        return ( <
-            div style = {
-                { marginLeft: 130 } } >
-            <
-            button className = "btn btn-lg btn-success"
-            disabled = {!this.props.value }
-            onClick = { this.props.startCountDown } > Start < /button> <
-            /div>
+        return (
+            <div style={{ marginLeft: 130 }}>
+                <button className="btn btn-lg btn-success" disabled={!this.props.value} onClick={this.props.startCountDown}>Start</button>
+            </div>
 
         );
     }
@@ -111,48 +99,33 @@ class App extends React.Component {
     render() {
         const clicked = this.state.isClicked;
         if (clicked) {
-            return ( <
-                div >
-                <
-                div className = "row" >
-                <
-                div className = "col-md-4" > < /div> <
-                div className = "col-md-4" >
+            return (
+                <div>
+                    <div className="row">
+                        <div className="col-md-4"></div>
+                        <div className="col-md-4">
 
-                <
-                p > 抽獎時間 < /p> <
-                Timer value = { this.state.value }
-                seconds = { this.state.seconds }
-                /> <
-                /div> <
-                /div> <
-                /div>
+                            <p> 抽獎時間 </p>
+                            <Timer value={this.state.value} seconds={this.state.seconds} />
+                        </div>
+                    </div>
+                </div>
             );
         } else {
-            return ( <
-                div >
-                <
-                div className = "row" >
-                <
-                div className = "col-md-4" > < /div> <
-                div className = "col-md-4" >
-                <
-                TimerInput value = { this.state.value }
-                handleChange = { this.handleChange }
-                /> <
-                Timer value = { this.state.value }
-                seconds = { this.state.seconds }
-                /> <
-                StartButton startCountDown = { this.startCountDown }
-                value = { this.state.value }
-                /> <
-                h2 > Lucky Person < /h2> <
-                h2 > { this.state.luckyWinner } < /h2>
+            return (
+                <div>
+                    <div className="row">
+                        <div className="col-md-4"></div>
+                        <div className="col-md-4">
+                            <TimerInput value={this.state.value} handleChange={this.handleChange} />
+                            <Timer value={this.state.value} seconds={this.state.seconds} />
+                            <StartButton startCountDown={this.startCountDown} value={this.state.value} />
+                            <h2>Lucky Person</h2>
+                            <h2>{this.state.luckyWinner} </h2>
 
-                <
-                /div> <
-                /div> <
-                /div>
+                        </div>
+                    </div>
+                </div>
             );
         }
     }
